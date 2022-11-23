54cd86af55e3c40812f66802834027581ad9e192
Vontell <sankarsh.rajeev@gmail.com>
2022-11-22 16:59:50 -0800

    Pushing to Heroku

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,16 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
+const PORT = process.env.PORT || 3306;
+
+
 // create connection to our db
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
-      port: 3306
+      port: PORT
     });
 
 module.exports = sequelize;
